88f19e21db73d9faa528d345e1c22ae44b096b82
YukaOtomo <182466613+yuka-om@users.noreply.github.com>
2024-10-07 15:25:08 +0900

    課題提出

---
+++ src/Chapter10.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chapter10 {
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+
+		// 以下記述
+
+		long i = list.stream()
+				.filter(t -> t.isDone() == false)
+				.count();
+		
+		System.out.println("未完了のタスクの個数は"+ i);
+		System.out.println("【未完了のタスクを昇順に並び替えて一覧表示】");
+
+
+		list.stream()
+		.filter(t -> t.isDone() == false)
+		.sorted((t1, t2) -> t1.compareTo(t2))
+		.forEach(System.out::println);
+
+
+
+
+
+	}
+}
+
--- src/Task.java
+++ src/Task.java
@@ -1,28 +1,43 @@
-
-
 import java.time.LocalDate;
 
-public class Task implements Comparable<Task>{
-
+//Comparableインタフェースを実装する
+public class Task implements Comparable<Task> {
 	private LocalDate date;
-	private String content;
+	private String task;
+	private boolean done; // 追加　完了はtrue, 未完了は false
 
-	public Task(LocalDate date, String content) {
+	public Task(LocalDate date, String task, boolean done) {
 		this.date = date;
-		this.content = content;
+		this.task = task;
+		this.done = done;
 	}
 
 	public LocalDate getDate() {
-		return this.date;
+		return date;
 	}
 
-	public String getContent() {
-		return this.content;
+	public String getTask() {
+		return task;
+	}
+
+	// boolean型のgetterメソッドはis～とすることもあります
+	public boolean isDone() {
+		return done;
+	}
+
+	// compareToメソッドをオーバーライドする
+	// 日付で並び替えるため、LocalDateのcompareToを使う
+
+
+
+	@Override
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
 	}
 
 	@Override
-	public int compareTo(Task otherTask) {
-		return this.date.compareTo(otherTask.date);
+	public String toString() {
+		return this.date + " " + this.task;
 	}
-
 }
+
